9a4e2e4ec02a352290c217400927cba61da18194
Bene-Tesla <benevaniosantos930@gmail.com>
2024-06-22 12:42:47 -0300

    feat: exception UI  in the game chess

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -11,19 +13,32 @@ public class App {
         ChessMatch chessMartch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMartch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            System.out.println(source);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMartch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println(source);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-            System.out.println(target);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+                System.out.println(target);
 
-            ChessPiece capturedPiece = chessMartch.performChessMove(source, target);
-            System.out.println("Captured piece: " + capturedPiece);
+                ChessPiece capturedPiece = chessMartch.performChessMove(source, target);
+                System.out.println("Captured piece: " + capturedPiece);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } finally {
+                System.out.println("Press Enter to continue...");
+                sc.nextLine();
+            }
+
             sc.close();
         }
 
